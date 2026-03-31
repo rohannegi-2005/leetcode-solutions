ebb4d098743470883dda338643f19d618bc3697a
Rohan Negi <151851829+rohannegi-2005@users.noreply.github.com>
2026-03-31 23:02:43 +0530

    Time: 2 ms (24.92%) | Memory: 19.3 MB (46.10%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,3 +1,18 @@
+# class Solution:
+#     def countGoodNumbers(self, n: int) -> int:
+#         def function(n):
+#             MOD = 10**9 + 7
+#             if n == 0 :
+#                 return 1
+#             if n % 2 == 0 :
+#                 return (4 * function(n-1)) % MOD
+#             else:
+#                 return (5 * function(n-1)) % MOD
+
+
+#         answer = function(n)
+#         return answer 
+
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         MOD = 10**9 + 7
@@ -18,3 +33,6 @@ class Solution:
         odd = n // 2
         
         return (power(5, even) * power(4, odd)) % MOD
+
+
+        
