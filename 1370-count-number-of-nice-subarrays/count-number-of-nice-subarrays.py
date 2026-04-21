13380b56d9ec33633a97e3165152a276448dd097
Rohan Negi <151851829+rohannegi-2005@users.noreply.github.com>
2026-04-22 00:01:39 +0530

    Time: 130 ms (44.97%) | Memory: 24.5 MB (91.81%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        length = len(nums)
+        for i in range(length):
+            if nums[i] % 2 == 0:
+                nums[i] = 0
+            else:
+                nums[i] = 1
+
+        print(nums)
+
+        prefix_map = {0: 1}   # prefix_sum → frequency
+        current_sum = 0
+        count = 0
+
+        for num in nums:
+            current_sum += num
+
+            if (current_sum - k) in prefix_map:
+                count += prefix_map[current_sum - k]
+
+            prefix_map[current_sum] = prefix_map.get(current_sum, 0) + 1
+
+        return count
+        
+
+
+
+        
