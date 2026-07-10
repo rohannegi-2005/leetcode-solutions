af0f4f8ffb961d88983250e2d6ec9e32181c9776
Rohan Negi <151851829+rohannegi-2005@users.noreply.github.com>
2026-07-10 23:44:34 +0530

    Time: 77 ms (80.54%) | Memory: 19.5 MB (84.64%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,21 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = [0] * 26
+        left = 0
+        maxFreq = 0
+        result = 0
+
+        for right in range(len(s)):
+            index = ord(s[right]) - ord('A')
+            count[index] += 1
+
+            maxFreq = max(maxFreq, count[index])
+
+            # check if window is invalid
+            while (right - left + 1) - maxFreq > k:
+                count[ord(s[left]) - ord('A')] -= 1
+                left += 1
+
+            result = max(result, right - left + 1)
+
+        return result
