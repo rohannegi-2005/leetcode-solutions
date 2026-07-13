0b2fcf60b39038700a5f393e255f2270504407df
Rohan Negi <151851829+rohannegi-2005@users.noreply.github.com>
2026-07-13 10:02:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.50%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,5 +1,25 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        # def generate(nums, current, index, result):
+        #     if index >= len(nums):
+        #         result.append(current[:])
+        #         return
+
+        #     # Exclude
+        #     generate(nums, current, index + 1, result)
+
+        #     # include
+        #     current.append(nums[index])
+        #     generate(nums, current, index + 1, result)
+        #     current.pop()
+
+        # def subsets_2(nums):
+        #     result = []
+        #     generate(nums, [], 0, result)
+        #     return result
+
+        # return (subsets_2(nums))
+
         def generate(index, current):
 
             if index == len(nums):
