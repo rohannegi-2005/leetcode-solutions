56099e00fdfa1c1654d9cd23b5655c7a1a64e875
Rohan Negi <151851829+rohannegi-2005@users.noreply.github.com>
2026-07-17 19:14:19 +0530

    Time: 11 ms (41.72%) | Memory: 19.1 MB (89.73%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        arr = []
+        for i in range(1,n+1):
+            arr.append(i)
+        
+        print(f"initial array:{arr}")
+
+        i = 0
+        while n > 1:
+            i = (i + k - 1) % n
+            arr.remove(arr[i])
+            n = n - 1
+
+        return arr.pop()
+
+        
+
+
+
+
+
+        
