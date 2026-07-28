11d5c973eed6eef43e2c91f8d0cf2a5bfa832089
Rohan Negi <151851829+rohannegi-2005@users.noreply.github.com>
2026-07-29 00:27:42 +0530

    Time: 354 ms (26.73%) | Memory: 21.6 MB (18.43%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -1,43 +1,72 @@
+# class Solution:
+#     def smallestPalindrome(self, s: str) -> str:
+
+#         dictionary = {}
+#         n = len(s)
+#         mid = n // 2
+
+#         start = 0
+#         end = n - 1
+
+#         for i in s:
+
+#             if i in dictionary:
+#                 dictionary[i] = dictionary[i] + 1
+#             else:
+#                 dictionary[i] = 1
+
+
+#         dictionary = dict(sorted(dictionary.items()))
+
+
+#         ans = [""] * n
+
+#         for key in dictionary.keys():
+#             print(key)
+#             if dictionary[key] % 2 != 0:
+#                 ans[mid] = key
+#                 dictionary[key] = dictionary[key] - 1
+
+#             for count in range(dictionary[key] // 2):
+#                 ans[start] = key
+#                 ans[end] = key
+#                 dictionary[key] = dictionary[key] - 2
+#                 start = start + 1
+#                 end = end - 1
+
+#         ans = "".join(ans)
+        
+#         return ans
+
+from collections import Counter
+
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
+        freq = Counter(s)
 
-        dictionary = {}
         n = len(s)
-        mid = n // 2
+        ans = [""] * n
 
         start = 0
         end = n - 1
+        mid = n // 2
 
-        for i in s:
+        for ch in sorted(freq.keys()):
+            count = freq[ch]
 
-            if i in dictionary:
-                dictionary[i] = dictionary[i] + 1
-            else:
-                dictionary[i] = 1
+            # Place the middle character (if odd frequency)
+            if count % 2 == 1:
+                ans[mid] = ch
 
+            # Place pairs from both ends
+            pairs = count // 2
+            for _ in range(pairs):
+                ans[start] = ch
+                ans[end] = ch
+                start += 1
+                end -= 1
 
-        dictionary = dict(sorted(dictionary.items()))
-
-
-        ans = [""] * n
-
-        for key in dictionary.keys():
-            print(key)
-            if dictionary[key] % 2 != 0:
-                ans[mid] = key
-                dictionary[key] = dictionary[key] - 1
-
-            for count in range(dictionary[key] // 2):
-                ans[start] = key
-                ans[end] = key
-                dictionary[key] = dictionary[key] - 2
-                start = start + 1
-                end = end - 1
-
-        ans = "".join(ans)
-        
-        return ans
-
+        return "".join(ans)
                 
 
 
