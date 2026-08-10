6680a9b39dddb44bb26bac7d8324cafd261cfb7d
Rohan Negi <151851829+rohannegi-2005@users.noreply.github.com>
2026-08-10 22:08:25 +0530

    Time: 246 ms (21.51%) | Memory: 52.9 MB (19.56%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,4 +1,25 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        n = len(piles)
+
+        dp = [[None] * (n+1) for _ in range(n+1)]
+
+        def solve(i, j):
+            if i > j :
+                return 0
+            
+            if dp[i][j] is not None:
+                return dp[i][j]
+
+            take_first = piles[i] - solve(i+1, j)
+
+            take_last = piles[j] - solve(i, j-1)
+
+            dp[i][j] =  max(take_first, take_last)
+
+            return dp[i][j]
+
+        return solve(0, n-1) > 0
+
+    
         
