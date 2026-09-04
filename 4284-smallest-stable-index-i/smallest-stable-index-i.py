6cc151485e535b50fff89f8c84fe8328398cc855
Rohan Negi <151851829+rohannegi-2005@users.noreply.github.com>
2026-09-05 02:00:12 +0530

    Time: 8 ms (36.12%) | Memory: 19.2 MB (70.81%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.py
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.py
@@ -4,18 +4,14 @@ class Solution:
         n = len(nums)
 
         stable = 0
-        mini = float('inf')
-        ans = -1
 
         for i in range(n):
             stable = max(nums[:i+1]) - min(nums[i:])
 
-            if stable <= k and mini > stable:   
-                mini = stable
-                ans = i
-                break
+            if stable <= k :  
+                return i 
 
-        return ans
+        return -1
 
 
 
